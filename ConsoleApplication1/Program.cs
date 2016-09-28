a9d0324af60ae41a965d31a2070ae3a02e2d33aa
h-Uche <winner.okeke.150302@unn.edu.ng>
2016-09-28 04:40:25 -0700

    Data Types; Operators first commit

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Numbers
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("int x = 10 ");
+            Console.WriteLine("if( x == 10 )");
+            Console.WriteLine(" x = 20 ");
+            Console.ReadLine();
+        }
+    }
+}
